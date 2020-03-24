6301cd85dd411e16fa5f240fb7d97ed7a5bbff29
IshTakkar <ishtakkar21@gmail.com>
2020-03-24 14:55:20 +0530

    a few visual changes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var campgrounds = [
     { name: "Salmon Creek", image: "http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_1008587612??hei=64&wid=64&qlt=50" },
     { name: "Paharganj", image: "https://cdn-blog.queensland.com/wp-content/uploads/2014/04/133096.jpg" },
     { name: "Lohagarh", image: "https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg" },
+    { name: "Salmon Creek", image: "http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_1008587612??hei=64&wid=64&qlt=50" },
+    { name: "Paharganj", image: "https://cdn-blog.queensland.com/wp-content/uploads/2014/04/133096.jpg" },
+    { name: "Lohagarh", image: "https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg" },
 ];
 
 app.use(bodyParser.urlencoded({ extended: true }));
